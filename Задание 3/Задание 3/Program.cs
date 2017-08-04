e702cb747667a4867875fa27b20b810856f77ade
Reinger <dvbolotov@edu.hse.ru>
2017-08-04 23:31:50 +0500

    Был написан ввод перемнных X и Y

---
+++ Задание 3/Задание 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static double ReadDouble(string S)
+        {
+            double X;
+            bool ok;
+            do
+            {
+                Console.Write(S);
+                ok = Double.TryParse(Console.ReadLine(), out X);
+                if (!ok)
+                {
+                    Console.WriteLine("Ошибка ввода. Необходимо ввести действительное число. Нажмите любую кнопку и повторите ввод.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            } while (!ok);
+
+            Console.Clear();
+            return X;
+        }
+
+        static void Main(string[] args)
+        {
+            double X = ReadDouble("X="), Y = ReadDouble("Y=");
+        }
+    }
+}
